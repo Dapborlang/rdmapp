7852827ec7b558d4f22230338936482cc35cb47f
Dapborlang <dapbor88@gmail.com>
2019-11-06 09:35:59 +0530

    Update Mailling System

--- config/mail.php
+++ config/mail.php
@@ -56,7 +56,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
+        'address' => env('MAIL_FROM_ADDRESS', 'automation@rdmapp.in'),
         'name' => env('MAIL_FROM_NAME', 'Automation System'),
     ],
 
