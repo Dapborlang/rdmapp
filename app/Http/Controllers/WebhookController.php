c3c99a59327868c52c25465ff03aab10e1b766d0
Dapborlang <dapbor88@gmail.com>
2019-11-25 23:30:15 +0530

    Webhook

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -10,17 +10,23 @@ class WebhookController extends Controller
 {
     public function status(Request $request)
     {
-    	if($request->token!="xtUvk2$#@")
-    	{
-    		return "Fail to load Webhook";
-    	}
-        $Status=SwitchStatus::findOrfail($request->id);
+    	if($json = json_decode(file_get_contents("php://input"), true)) {
+		     $data = $json;
+		 } else {
+		     $data = $_POST;
+		 }
+        if($data['token']!="xtUvk2$#@")
+        {
+            return "Fail to load Webhook";
+        }
+        
+        $Status=SwitchStatus::findOrfail($data['id']);
 
         $client = new \GuzzleHttp\Client();
-        $url='http://'.$Status->ipAddress->ip.':'.$Status->port.'/'.$Status->pin.$request->status;              
+        $url='http://'.$Status->ipAddress->ip.':'.$Status->port.'/'.$Status->pin.$data['status'];              
         $response = $client->request('GET', $url);
 
-        $Status->status=$request->status;
+        $Status->status=$data['status'];
         // $Status->flag='U';
         $Status->save();
         return $response;
